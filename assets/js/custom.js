3e1245f6530a2f729e0970470ed4bc50f632aefb
Belal khan <belalkhan.dev@gmail.com>
2021-01-01 22:52:42 +0600

    website initial file upload

---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+(function ($) {
+    "use-strict"
+
+    // document ready
+    jQuery(document).ready(function () {
+        
+        
+
+    });
+
+    // window on load
+    jQuery(window).on('load', function () {
+
+        
+    });
+
+
+
+}(jQuery));
